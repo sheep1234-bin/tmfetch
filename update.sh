82767f3cd10313924ad4ab31f6dbc3e7658703f0
Μιψηαελ Τεαη <nautharchiegit@gmail.com>
2026-05-28 20:43:57 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-rm /usr/local/bin/tmfetch && curl -LO https://github.com/sheep1234-bin/tmfetch/raw/main/tmfetch && chmod +x tmfetch && mv tmfetch /usr/local/bin && echo "Updated Successfully!"
+rm /usr/local/bin/tmfetch && curl https://github.com/sheep1234-bin/tmfetch/raw/main/tmfetch && chmod +x tmfetch && mv tmfetch /usr/local/bin && echo "Updated Successfully!"
